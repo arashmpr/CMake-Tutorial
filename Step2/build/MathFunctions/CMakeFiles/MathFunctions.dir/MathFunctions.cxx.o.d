0d0a835f505c1feac472dd7e6e77fd64860af687
arashmpr <arashmdp@gmail.com>
2024-01-24 13:36:07 +0330

    modified the step2 tutorial

---
+++ Step2/build/MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o.d
@@ -0,0 +1,4 @@
+MathFunctions/CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o: \
+  /Users/nebula/Documents/Codes/Personal/CMake-Tutorial/Step2/code/MathFunctions/MathFunctions.cxx \
+  /Users/nebula/Documents/Codes/Personal/CMake-Tutorial/Step2/code/MathFunctions/MathFunctions.h \
+  /Users/nebula/Documents/Codes/Personal/CMake-Tutorial/Step2/code/MathFunctions/mysqrt.h
